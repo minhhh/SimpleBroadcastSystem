8c6ced40bf04926d9832d07ec73ff68905864261
Ha.Minh <minhhh@minhhuyha.info>
2017-09-11 13:53:16 +0700

    Initial Update

---
+++ Assets/SimpleBroadcastSystem/Plugins/SimpleBroadcastSystem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SimpleBroadcastSystem
+{
+    protected class BroadcastEvent : UnityEvent <object>
+    {
+    }
+
+    protected Dictionary<string, BroadcastEvent> subscriberLists = new Dictionary<string, BroadcastEvent> ();
+
+    public void AddSubscriber (string eventName, UnityAction <object> callback)
+    {
+        BroadcastEvent unityEvent = null;
+        if (!subscriberLists.TryGetValue (eventName, out unityEvent)) {
+            subscriberLists [eventName] = new BroadcastEvent ();
+        }
+        subscriberLists [eventName].AddListener (callback);
+    }
+
+    public void RemoveSubscriber (string eventName, UnityAction <object> callback)
+    {
+        try {
+            BroadcastEvent unityEvent = null;
+            if (subscriberLists.TryGetValue (eventName, out unityEvent)) {
+                unityEvent.RemoveListener (callback);
+            }
+        } catch (Exception e) {
+            Debug.LogWarning (string.Format ("{0}::RemoveSubscriber Error {1}", this.GetType ().FullName, e.Message));
+        }
+    }
+
+    public void Broadcast (string eventName, object parameter = null)
+    {
+        BroadcastEvent unityEvent = null;
+        if (subscriberLists.TryGetValue (eventName, out unityEvent)) {
+            unityEvent.Invoke (parameter);
+        }
+    }
+}
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# SimpleBroadcastSystem
+
+SimpleBroadcastSystem is a minimal implementation of a MessageBus or Pub/Sub pattern. It's up to the calling code to create a static instance of `SimpleBroadcastSystem`
+
+To include SimpleBroadcastSystem into your project, you can use `npm` method of unity package management described [here](https://github.com/minhhh/UBootstrap).
+
+## Changelog
+
+**0.0.1**
+
+* Initial commit
+
+<br/>
+
